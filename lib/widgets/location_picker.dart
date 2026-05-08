da0672436e400b839b61cc7e73d629979b0236a7
Fadila Mohammed <Fadilamohammed657@gmail.com>
2026-05-08 21:26:20 +0100

    update admin, security and system widgets

--- lib/widgets/location_picker.dart
+++ lib/widgets/location_picker.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../config/theme.dart';
+// Student frontend update by Fadila Mohammed
 
+  // This widget is the root of your application.
 class LocationPicker extends StatefulWidget {
   final String? selectedLocation;
   final Function(String) onLocationSelected;
